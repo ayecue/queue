2b68d94e7ef42be7e1f25500ec7b0f29a2f93915
ayecue <soerenwehmeier@googlemail.com>
2023-08-29 19:09:28 +0200

    0.1.11 use function to manage state in task

--- src/libs/task.ts
+++ src/libs/task.ts
@@ -30,39 +30,64 @@ export default class Task extends EventEmitter {
     this.callback = options.callback;
   }
 
+  private transitionToState(state: TaskState) {
+    switch (state) {
+      case TaskState.Running: {
+        if (this.state !== TaskState.Pending) {
+          throw new Error(
+            `Cannot transition task to ${state} when not pending.`
+          );
+        }
+        break;
+      }
+      case TaskState.Canceled:
+        if (this.state === TaskState.Canceled) {
+          throw new Error(
+            `Cannot transition task to ${state} that has already been canceled.`
+          );
+        }
+      case TaskState.Fulfilled:
+      case TaskState.Failed: {
+        if (this.state === TaskState.Fulfilled) {
+          throw new Error(
+            `Cannot transition task to ${state} that has already been fulfilled.`
+          );
+        }
+        if (this.state === TaskState.Failed) {
+          throw new Error(
+            `Cannot transition task to ${state} that has already failed.`
+          );
+        }
+        break;
+      }
+    }
+
+    this.state = state;
+  }
+
   async run(): Promise<void> {
     try {
-      if (this.state !== TaskState.Pending) {
-        throw new Error('Cannot only run pending task.');
-      }
-
-      this.state = TaskState.Running;
+      this.transitionToState(TaskState.Running);
       this.emit('start', this);
 
       const payload = await this.callback(this);
 
-      if (this.state !== TaskState.Running) {
-        throw new Error(
-          'Cannot only return result when state was not canceled.'
-        );
+      if (this.state === TaskState.Canceled) {
+        this.transitionToState(TaskState.Fulfilled);
+        this.emit('done', this, null);
+        return;
       }
 
-      this.state = TaskState.Fulfilled;
+      this.transitionToState(TaskState.Fulfilled);
       this.emit('done', this, payload);
     } catch (err) {
-      if (this.state !== TaskState.Failed) {
-        this.state = TaskState.Failed;
-        this.emit('error', this, err);
-      }
+      this.transitionToState(TaskState.Failed);
+      this.emit('error', this, err);
     }
   }
 
   cancel() {
-    if (this.state !== TaskState.Pending && this.state !== TaskState.Running) {
-      throw new Error('Cannot cancel non pending or non running task.');
-    }
-
-    this.state = TaskState.Canceled;
+    this.transitionToState(TaskState.Canceled);
   }
 
   getState(): TaskState {
